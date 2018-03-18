44b516eafa5e3c505f7354d843191e0545653927
Dagon <31436836+Dagon0577@users.noreply.github.com>
2018-03-19 00:23:36 +0800

    刷题第九天

---
+++ 606. Construct String from Binary Tree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public String tree2str(TreeNode t) {
+        StringBuilder s=new StringBuilder();
+        append(t, s);
+        return s.toString();
+    }
+    public void append(TreeNode t,StringBuilder s){
+        if(t==null)
+            return ;
+        s.append(t.val);
+        if(t.left!=null&&t.right!=null){
+            s.append("(");
+            append(t.left,s);
+            s.append(")(");
+            append(t.right,s);
+            s.append(")");            
+        }
+        else if(t.left!=null&&t.right==null){
+            s.append("(");
+            append(t.left,s);
+            s.append(")");
+        }
+        else if(t.left==null&&t.right!=null){
+            s.append("()(");
+            append(t.right,s);
+            s.append(")");
+        }
+        else return;
+    }
+}
+
+
