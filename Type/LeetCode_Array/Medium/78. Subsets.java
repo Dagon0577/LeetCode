78c20dbffa9992b12b45b966ee07ca4766578705
huangganyan <huangganyan@hotpu.cn>
2019-05-16 20:19:13 +0800

    打卡3

---
+++ Type/LeetCode_Array/Medium/78. Subsets.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> temp = new ArrayList<Integer>();
+        dfs(res, temp, nums, 0);
+        return res;
+    }
+    private void dfs(List<List<Integer>> res, List<Integer> temp, int[] nums, int index) {
+        res.add(new ArrayList<Integer>(temp));
+        for (int i = index;i < nums.length; i++){
+            temp.add(nums[i]);
+            dfs(res,temp,nums,i+1);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
