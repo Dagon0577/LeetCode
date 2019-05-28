44275603dc9ba113bfc7688778a037f253734bbe
Dagon0577 <275253612@qq.com>
2019-05-28 19:10:24 +0800

    打卡7
    打卡7

---
+++ Type/LeetCode_Array/Medium/667. Beautiful Arrangement II.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] constructArray(int n, int k) {
+        int[] result = new int[n];
+        int even = 1;
+        int odd = k+1;
+        for(int i = 0; i <= k; i+=2){
+            result[i] = even++; 
+        }
+        for(int i = 1; i <= k; i+=2){
+            result[i] = odd--;
+        }
+        for(int i = k + 1; i < n; ++i){
+            result[i] = i + 1;
+        }
+        return result;
+    }
+}
