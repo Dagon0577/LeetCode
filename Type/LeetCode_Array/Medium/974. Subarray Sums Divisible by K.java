5de2157027946d575619b36c7795aacfa230d3c3
Dagon0577 <dagon0577@gmail.com>
2019-10-09 17:59:09 +0800

    day 25

---
+++ Type/LeetCode_Array/Medium/974. Subarray Sums Divisible by K.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int subarraysDivByK(int[] A, int K) {
+        int N = A.length, sum = 0, ans = 0;
+        int[] map = new int[K];
+        map[0] = 1;
+        for (int i = 1; i <= N; i++) {
+            sum = sum + A[i-1]; 
+            int key = (sum % K + K) % K;
+            ans += map[key];
+            map[key]++;
+        }
+        return ans;
+    }
+}
