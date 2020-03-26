7f09c3714b53f60c72807d299757d3de3cb1fcd5
Dagon0577 <dagon0577@gmail.com>
2020-03-27 05:50:59 +0800

    5.

---
+++ LeetcodeTop100/5. 最长回文子串.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1)
+            return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = judge(s, i, i);// 中心为一个数
+            int len2 = judge(s, i, i + 1);// 中心为两个数
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int judge(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+}
