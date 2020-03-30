faf92e8d462f6f00e834bd5dc27061cee03c1a9d
Dagon0577 <dagon0577@gmail.com>
2020-03-31 05:23:46 +0800

    fix 32.(dp)

--- LeetcodeTop100/32. 最长有效括号.java
+++ LeetcodeTop100/32. 最长有效括号.java
@@ -4,9 +4,9 @@ class Solution {
         int dp[] = new int[s.length()];
         for (int i = 1; i < s.length(); i++) {
             if (s.charAt(i) == ')') {
-                if (s.charAt(i) == '(') {
+                if (s.charAt(i - 1) == '(') {
                     dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
-                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                } else if (i - dp[i - 1] - 1 >= 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
                     // ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0)即为求上面的情况。
                     dp[i] = dp[i - 1] + 2 + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0);
                     // 结果=前面已经存在的字符串S1+现在拼入的字符串+之前还可能存在的字符串s2。
