666800b65fd9f6c9567ce3df9b0f250580950ea3
Dagon0577 <dagon0577@gmail.com>
2020-04-12 13:08:03 +0800

    184周赛

---
+++ leetcode周赛/第184周/5381. 查询带键的排列.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] processQueries(int[] queries, int m) {
+        int n = queries.length;
+        int[] res = new int[n];
+        int[] P = new int[m];
+        for(int i = 0; i < m; i++){
+            P[i] = i + 1;
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(P[j] == queries[i]) {
+                    res[i] = j;
+                    sort(P,0,j);
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+    private void sort(int[] list,int start,int end){
+        int tmp = list[end];
+        for(int i = end; i > start; i--){
+            list[i] = list[i - 1];
+        }
+        list[start] = tmp;
+    }
+}
---
+++ leetcode周赛/第184周/5382. HTML 实体解析器.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String entityParser(String text) {
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i < text.length(); i++){
+            char c = text.charAt(i);
+            if(c == '&'){
+                for(int j = i + 1; j < text.length(); j++){
+                    if(text.charAt(j) == ';'){
+                        String tmp = text.substring(i,j+1);
+                        if(tmp.equals("&quot;")){
+                            res.append("\"");
+                        } else if(tmp.equals("&apos;")){
+                            res.append("'");
+                        } else if(tmp.equals("&amp;")) {
+                            res.append("&");
+                        } else if(tmp.equals("&gt;")) {
+                            res.append(">");
+                        } else if(tmp.equals("&lt;")) {
+                            res.append("<");
+                        } else if(tmp.equals("&frasl;")) {
+                            res.append("/");
+                        } else {
+                            res.append(tmp);
+                        }
+                        i = j;
+                        break;
+                    }
+                }
+            } else {
+                res.append(c);
+            }
+        }
+        return res.toString();
+    }
+}
