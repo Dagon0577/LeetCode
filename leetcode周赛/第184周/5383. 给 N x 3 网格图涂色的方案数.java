24ea8e7d12cd3b655a4ddabf7e497322f76a33d9
Dagon0577 <dagon0577@gmail.com>
2020-04-12 13:21:08 +0800

    添加5383思路

--- leetcode周赛/第184周/5383. 给 N x 3 网格图涂色的方案数.java
+++ leetcode周赛/第184周/5383. 给 N x 3 网格图涂色的方案数.java
@@ -10,3 +10,7 @@ class Solution {
         return (int) (dp[n - 1][0] + dp[n - 1][1]) % (1000000007);
     }
 }
+/*
+dp[i][0] 为abc情况下，下一行有四种情况即：[cab,bca]和[bab,bcb]
+dp[i][1] 为aba情况下，下一行有五种情况即：[cab,bac]和[bab,bcb,cac]
+*/
