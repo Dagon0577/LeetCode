f85210ac8021f907a519a4096e5041e220823cce
Dagon0577 <dagon0577@gmail.com>
2020-04-15 00:10:54 +0800

    84. （优化）

--- LeetcodeTop100/84. 柱状图中最大的矩形.java
+++ LeetcodeTop100/84. 柱状图中最大的矩形.java
@@ -1,12 +1,16 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
         int res = 0;
-        for(int i = 0; i < heights.length;i++){
-            int minHeight = Integer.MAX_VALUE;
-            for(int j = i; j < heights.length;j++){
-                minHeight = Math.min(minHeight, heights[j]);
-                res = Math.max(res,minHeight * (j - i + 1));
+        for (int i = 0; i < heights.length; i++) {
+            while (stack.peek() != -1 && heights[i] < heights[stack.peek()]) {
+                res = Math.max(res, heights[stack.pop()] * (i - 1 - stack.peek()));
             }
+            stack.push(i);
+        }
+        while (stack.peek() != -1) {
+            res = Math.max(res, heights[stack.pop()] * (heights.length - 1 - stack.peek()));
         }
         return res;
     }
